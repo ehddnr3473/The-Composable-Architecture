0ee52a13bbe3aa9f3ebdb194725d643436ef038b
Dongwook Kim <ehddnr73@naver.com>
2023-07-01 19:03:59 +0900

    Project created.

---
+++ TheComposableArchitecture/TheComposableArchitecture/TheComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheComposableArchitectureApp.swift
+//  TheComposableArchitecture
+//
+//  Created by 김동욱 on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TheComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
