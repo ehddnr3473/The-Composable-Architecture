bede9ae03d640a303ea77edb1daa603c6ff54b5b
dong wook kim <ehddnr73@naver.com>
2023-07-03 18:34:04 +0900

    설명 주석 추가 및 다른 방식으로 뷰 구현하기 위해 코드 수정
    - alert 관련 코드 주석 처리

--- TheComposableArchitecture/TheComposableArchitecture/CounterFeature.swift
+++ TheComposableArchitecture/TheComposableArchitecture/CounterFeature.swift
@@ -15,17 +15,19 @@ struct CounterFeature: ReducerProtocol {
     /// 단일 정수형 데이터로 구성된 상태
     struct State: Equatable {
         var count = 0;
-        var numberFactAlert: String?
+        var fact: String?
+        var isLoading = false
+//        var numberFactAlert: String?
     }
     
     /// Feature에서 사용자가 수행할 수 있는 모든 액션을 기술하는 타입(일반적으로 enum)
     /// Action의 case 이름은 로직[ ex) incrementCount ]보다는 사용자가 UI에서 행하는 작업에 초점을 맞춰서 지을 것.
     enum Action: Equatable {
-        case factAlertDismissed
+//        case factAlertDismissed
         case decrementButtonTapped
         case incrementButtonTapped
         case numberFactButtonTapped
-        case numberFactResponse(String)
+        case numberFactResponse(String) // associated value: String that fetched from the network
     }
     
     /// 현재 값에서 다음 값으로 상태를 변화시키기 위한 메서드. 외부에서 실행하기를 원하는 effect를 반환함.
@@ -36,8 +38,13 @@ struct CounterFeature: ReducerProtocol {
     /// - Returns: 외부에서 실행할 Effect를 표현하는 값
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
-        case .factAlertDismissed:
-            state.numberFactAlert = nil
+//        case .factAlertDismissed:
+//            state.numberFactAlert = nil
+//            return .none
+            
+        case .decrementButtonTapped:
+            state.count -= 1
+            state.fact = nil
             /*
              An effect that does nothing and completes immediately.
              Useful for situations where you must return an effect, but you don’t need to do anything.
@@ -47,26 +54,36 @@ struct CounterFeature: ReducerProtocol {
              */
             return .none
             
-        case .decrementButtonTapped:
-            state.count -= 1
-            return .none
-            
         case .incrementButtonTapped:
             state.count += 1
+            state.fact = nil
             return .none
             
         case .numberFactButtonTapped:
+            state.fact = nil
+            state.isLoading = true
+            /*
+             * TCA는 상태의 단순하고 순수한 변환과 복잡한 side-effect(API request와 같은 외부와의 소통)를 분리함.
+             * 이 블록에서 직접 URLSession을 사용해서 비동기 작업을 할 수 없음.
+             * 대신 TCA 라이브러리에서 side-effect를 수행할 EffectTask라는 도구를 제공
+             */
             return .run { [count = state.count] send in
+                // Do async work in here, and send actions back into the system.
                 let (data, _) = try await URLSession.shared.data(
                     from: URL(string: "http://numbersapi.com/\(count)/trivia")!
                 )
-                await send(
-                    .numberFactResponse(String(decoding: data, as: UTF8.self))
-                )
+                
+                let fact = String(decoding: data, as: UTF8.self)
+                await send(.numberFactResponse(fact))
+//                await send(
+//                    .numberFactResponse(String(decoding: data, as: UTF8.self))
+//                )
             }
             
         case let .numberFactResponse(fact):
-            state.numberFactAlert = fact
+            state.fact = fact
+            state.isLoading = false
+//            state.numberFactAlert = fact
             return .none
         }
     }
